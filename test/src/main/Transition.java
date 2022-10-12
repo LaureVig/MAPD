2c305e5d0a0df0efa6ec1c4fff86aa8f31c0baa9
Laure Vigouroux <103105302+LaureVig@users.noreply.github.com>
2022-10-12 15:41:07 +0200

    Methods Transition created (not implemented

--- test/src/main/Transition.java
+++ test/src/main/Transition.java
@@ -1,5 +1,35 @@
 package main;
 
 public class Transition {
-
+	private int id;
+	private boolean activable;
+	
+	/**
+	 * Constructor of Transition
+	 * @param id
+	 * @param activable
+	 */
+	public Transition(int id, boolean activable) {
+		this.id = id;
+		this.activable = activable;
+	}
+	
+	// Getters and Setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public boolean isActivable() {
+		return activable;
+	}
+	public void setActivable(boolean activable) {
+		this.activable = activable;
+	}
+	
+	// Methods
+	public void doTransition() {
+		
+	}
 }
