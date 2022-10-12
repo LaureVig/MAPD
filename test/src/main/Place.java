ff89f954c5ff38fa04b223a266636825eeb45be3
Laure Vigouroux <103105302+LaureVig@users.noreply.github.com>
2022-10-12 15:30:17 +0200

    Create methods for Place and Class Transition

--- test/src/main/Place.java
+++ test/src/main/Place.java
@@ -1,5 +1,34 @@
 package main;
 
 public class Place {
-
+	private int id;
+	private int numberOfTokens;
+	
+	/**
+	 * Constructor of Place
+	 * @param id
+	 * @param numberOfTokens
+	 */
+	public Place(int id, int numberOfTokens) {
+		this.id = id;
+		this.numberOfTokens = numberOfTokens;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getNumberOfTokens() {
+		return numberOfTokens;
+	}
+	public void setNumberOfTokens(int numberOfTokens) {
+		this.numberOfTokens = numberOfTokens;
+	}
+	
+	public boolean checkActivable() {
+		
+		return true;
+	}
 }
