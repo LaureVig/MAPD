13f9c05a96fecf30e583d14c3283cf2370bab39a
Laure Vigouroux <103105302+LaureVig@users.noreply.github.com>
2022-10-12 16:28:14 +0200

    Network

--- test/src/main/Network.java
+++ test/src/main/Network.java
@@ -43,7 +43,69 @@ public class Network {
 		this.transitions = transitions;
 	}
 	
-	public void addPlace(int id) {
+	public void addPlace(Place place) {
+		if (!(places.contains(place))) {
+			this.places.add(place);
+		}
+	}
+	
+	public void deletePlace(Place place) {
+		this.places.removeFirstOccurrence(place);
+	}
+	
+	public Place findPlace(int index) {
+		return places.get(index);
+	}
+	
+	public void addTransition(Transition transition) {
+		if(!(transitions.contains(transition))) {
+			transitions.add(transition);
+		}
+	}
+	
+	public void deleteTransition(Transition transition) {
+		this.transitions.removeFirstOccurrence(transition);
+	}
+	
+	public Transition findTransition(int index) {
+		return transitions.get(index);
+	}
+	
+	public void addEdge(Edge edge) {
+		if(!(edges.contains(edge))) {
+			edges.add(edge);
+		}
+	}
+	
+	public void deleteEdge(Edge edge) {
+		this.edges.removeFirstOccurrence(edge);
+	}
+	
+	public Edge findEdge(int index) {
+		return edges.get(index);
+	}
+	
+	public void editWeight(Edge edge, int weight) {
+		edge.setWeight(weight);
+	}
+	
+	public void changeNumberOfTokens(Place place, int numberOfTokens) {
+		place.setNumberOfTokens(numberOfTokens);
+	}
+	
+	public void step() {
 		
+	}
+	public LinkedList<Transition> getFirable() {
+		int size=transitions.size();
+		LinkedList<Transition> fireable = new LinkedList<Transition>();
+		for(int i=0;i<size;i++) {
+			Transition transition = this.transitions.get(i);
+//			transition.checkActivable();
+//			if (transition.isActivable()) {
+//				fireable.add(transition);
+//			}
+		}
+		return fireable;
 	}
 }
